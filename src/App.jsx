26c13751ec7a15348174c85e331874044283727c
Ganesh <ganesh.oficial158@gmail.com>
2024-07-15 02:02:03 +0530

    Project section update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './Components/Home/Home.jsx'
 import Projects from './Components/Projects/Projects.jsx'
 import About from './Components/About/About.jsx'
 import Footer from './Components/Footer/Footer.jsx';
+import Resume from './Components/Resume/Resume.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
         {/* Add more routes as needed */}
       </Routes>
       <Footer />
--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import './Resume.css'
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import pdfFile from "../../Assets/Soumyajit_Behera-BIT_MESRA.pdf"; // Adjust path as per your project structure
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@2.13.0/build/pdf.worker.min.js`;
 
 function Resume() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
   return (
     <div>
-      
+      <Container fluid className="resume-section">
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdfFile}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        <Row className="resume">
+          <Document file={pdfFile} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdfFile}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Resume
+export default Resume;
